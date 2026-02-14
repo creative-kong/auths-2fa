0fcf49d7ccc2a7a55e7941bc9895526ba8152914
Pancha Nuamsahwat <pancha2539@icloud.com>
2026-02-14 19:16:57 +0700

    change back end language to rust

---
+++ api/src/routes/routes.rs
@@ -0,0 +1,6 @@
+use axum:: { routing::get, Router };
+
+pub fn app () -> Router {
+    Router::new()
+        .route("/", get(|| async { "Hello World" }))
+}
--- api/src/utils/get-env.ts
+++
@@ -1,8 +0,0 @@
-export const getEnv = (key : string, defaultValue : string = "") : string => {
-    const value = process.env[key]
-    if (!value) {
-        if (defaultValue) return defaultValue
-        throw new Error(`environment variable ${key} not set.`)
-    }
-    return value
-}
